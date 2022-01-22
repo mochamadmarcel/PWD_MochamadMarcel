ffff183a56ddd4c807a8e2849919965ef9783681
mochamad marcel <mochamadmarcel@gmail.com>
2022-01-22 13:16:06 +0700

    mengupload semua tugas

---
+++ tugas 3/Pertemuan 5/latihan_8.php
@@ -0,0 +1,23 @@
+<?php
+$ball = 'purple';
+//Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+if ($ball == 'red') {
+$hasil = $ball;
+} elseif ($ball == 'yellow') {
+$hasil = $ball;
+} elseif ($ball == 'blue') {
+$hasil = $ball;
+} elseif ($ball == 'green') {
+$hasil = $ball;
+} elseif ($ball == 'purple') {
+$hasil = $ball;
+} else {
+$hasil = $ball;
+}
+echo "red box : $hasil <br>\n";
+echo "yellow box :$hasil <br>\n";
+echo "blue box : $hasil <br>\n";
+echo "green box : $hasil <br>\n";
+echo "purple box : $hasil <br>\n";
+echo "colorless box : $hasil <br>\n";
+?>
